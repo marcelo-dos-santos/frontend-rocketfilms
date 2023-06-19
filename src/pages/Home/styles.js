c99a70f9476d1e84095b0bba74b4b4e3571d0d00
Marcelo dos Santos <marcelinho.cost@gmail.com>
2023-06-19 19:05:55 -0300

    a

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: grid;
-    grid-template-rows: 105px 128px auto 64px;
+    grid-template-rows: 105px 128px auto 30px;
     grid-template-areas: 
     "header"
     "userinterface"
